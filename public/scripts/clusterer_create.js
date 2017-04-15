3da2e6c17d032708abd9f7e00a6d5ad763f00c78
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2017-04-15 17:27:17 +0300

    made map of pps

---
+++ public/scripts/clusterer_create.js
@@ -0,0 +1,73 @@
+var points=[];
+
+function chooseBK(){
+	$('#bkSelect').on('change', function(e){
+		e.preventDefault();
+		var bk = $(this).val();
+		getPPSCoordinates(bk);
+	});
+}
+
+function getPPSCoordinates(bk){
+	points=[];
+	$('#map').empty();
+	$.ajax({
+		url: '/bkpps/ppscoordinates' + bk + '.json',
+		dataType: 'json',
+		success: function(data){
+			data.forEach(dat=>{
+				points.push({bk: dat.bk, coords:dat.data.geometry.coordinates});
+			})
+			drawMap();
+		}		
+	});
+
+}
+
+function drawMap(){
+	var myMap = new ymaps.Map('map', {
+            center: [55.751574, 37.573856],
+            zoom: 9,
+            behaviors: ['default', 'scrollZoom']
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+            clusterer = new ymaps.Clusterer({
+            preset: 'islands#invertedVioletClusterIcons',
+            groupByCoordinates: false,
+            clusterDisableClickZoom: true,
+            clusterHideIconOnBalloonOpen: true,
+            geoObjectHideIconOnBalloonOpen: false
+        }),
+            getPointData = function (index, bk) {
+            return {
+                balloonContentBody: 'ППС ' + bk,
+                clusterCaption: 'ППС ' + bk
+            };
+        },
+            getPointOptions = function () {
+            return {
+                preset: 'islands#violetIcon'
+            };
+        },
+        geoObjects = [];
+    for(var i = 0, len = points.length; i < len; i++) {
+        geoObjects[i] = new ymaps.Placemark(points[i].coords, getPointData(i, points[i].bk), getPointOptions());
+    }
+    clusterer.options.set({
+        gridSize: 80,
+        clusterDisableClickZoom: false
+    });
+    clusterer.add(geoObjects);
+    myMap.geoObjects.add(clusterer);
+
+    myMap.setBounds(clusterer.getBounds(), {
+        checkZoomRange: false
+    });
+}
+
+ymaps.ready(function () {
+	//todo - function of getting pps coordinates from db, depending on filter oresets
+	chooseBK();
+ 
+});
